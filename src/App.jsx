1f258dac7c75e2ffbdaaa726ddd10644679ad128
Worker Agent <worker@example.com>
2026-02-12 19:51:36 +0530

    Initial commit: Maintenance page with animations

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import MaintenancePage from './components/MaintenancePage'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <MaintenancePage />
+    </div>
+  )
+}
+
+export default App
